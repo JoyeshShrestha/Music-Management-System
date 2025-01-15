385eb6279e9ce6c4abd84fda3625d00750662c74
Joyesh Shrestha <sjoyesh2000@gmail.com>
2025-01-15 17:24:56 +0545

    CRU on user withh pagination

--- main.py
+++ main.py
@@ -119,6 +119,48 @@ def check_email_exists(email):
         print(f"An error occurred: {e}")
         return False
 
+def update_data_user(id,first_name,last_name,date_of_birth,gender,email,password,phone,address):
+    try:
+        # Establish database connection
+        connection = get_db_connection()
+        cursor = connection.cursor()
+
+        # Use parameterized queries to prevent SQL injection
+        update_user_query = """
+                                UPDATE user
+                                SET first_name = %s,
+                                    last_name = %s,
+                                    email = %s,
+                                    password = %s,
+                                    phone = %s,
+                                    dob = %s,
+                                    gender = %s,
+                                    address = %s
+                                WHERE id = %s;
+                                """
+
+
+        # Execute the query with parameters
+        cursor.execute(update_user_query, (first_name, last_name, email, password, phone, date_of_birth, gender, address, id))
+
+        # Commit the transaction
+        connection.commit()
+
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()
+
+        # Flash success message and redirect to login
+        flash("User edited successfully!")
+        return redirect(url_for("get_all_users"))
+
+
+    except Exception as e:
+        # Handle exceptions and flash error message
+        flash(f"An error occurred: {e}")
+        return redirect(url_for("get_all_users"))
+
+
 
 def insert_data_user(first_name, last_name, date_of_birth, gender, email, password, phone, address):
     try:
@@ -144,41 +186,42 @@ def insert_data_user(first_name, last_name, date_of_birth, gender, email, passwo
 
         # Flash success message and redirect to login
         flash("User inserted successfully!")
-        return redirect(url_for("login"))
+        return True
 
     except Exception as e:
         # Handle exceptions and flash error message
         flash(f"An error occurred: {e}")
-        return redirect(url_for("register"))
+        return False
                  
 
 
-def get_data_users():
+def get_data_users(page,per_page):
     try:
         # Establish database connection
         connection = get_db_connection()
         cursor = connection.cursor(DictCursor)
+        cursors = connection.cursor()
 
-        # Use parameterized queries to prevent SQL injection
-        select_table_query = """
-        SELECT * FROM user ;
-        """
-
-        # Execute the query with parameters
-        cursor.execute(select_table_query)
-
-        # Commit the transaction
-        connection.commit()
-        all_users_tuple = cursor.fetchall()
-
+        offset = (page - 1) * per_page
+        query = "SELECT * FROM user LIMIT %s OFFSET %s"
         
+        # Execute query with the limit and offset
+        cursor.execute(query, (per_page, offset))
+        users = cursor.fetchall()
+
+        # Count total users
+        total_query = "SELECT COUNT(*) FROM user"
+        cursors.execute(total_query)
+        total_users =  cursors.fetchone()[0]
+
 
         # Close the cursor and connection
         cursor.close()
         connection.close()
 
     
-        return all_users_tuple
+        return users, total_users
+
 
     except Exception as e:
         # Handle exceptions and flash error message
@@ -219,6 +262,37 @@ def delete_user_query(user_id):
         flash(f"An error occurred: {e}")
         return redirect(url_for("get_all_users"))
 
+def get_user_data(user_id):
+    try:
+        # Establish database connection
+        connection = get_db_connection()
+        cursor = connection.cursor(DictCursor)
+
+        # Use parameterized queries to prevent SQL injection
+        select_table_query = """
+        SELECT * FROM user WHERE id = %s ;
+        """
+
+        # Execute the query with parameters
+        cursor.execute(select_table_query,user_id)
+
+        # Commit the transaction
+        connection.commit()
+        user_tuple = cursor.fetchone()
+
+        
+
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()
+
+    
+        return user_tuple
+
+    except Exception as e:
+        # Handle exceptions and flash error message
+        flash(f"An error occurred: {e}")
+        return redirect(url_for("get_all_users"))
 
 @app.route('/',methods=['GET','POST'])
 def register():
@@ -248,9 +322,9 @@ def register():
             address = request.form["Address"]
             password = generate_password_hash(request.form["password"],method='pbkdf2:sha256',salt_length=8)
 
-            insert_data_user(first_name,last_name,date_of_birth,gender,email,password,phone,address)
-
-            return render_template("login.html")
+            is_insert = insert_data_user(first_name,last_name,date_of_birth,gender,email,password,phone,address)
+            if is_insert:
+                return render_template("login.html")
     return render_template("register.html")
 
 
@@ -292,22 +366,91 @@ def dashboard():
 
 
 
-@app.route('/dashboard/users')
-@login_required
-def get_all_users():
-    
-    users = get_data_users()    
-    if current_user.is_authenticated:
-        return render_template("user.html", all_users=users,logged_in = current_user.is_authenticated,login_id = current_user.id)
-
-    return render_template("login.html", all_users=users,logged_in = current_user.is_authenticated)
-
 
 @app.route('/dashboard/users/delete/<int:user_id>')
 @login_required
 def delete_user(user_id):
     delete_user_query(user_id)
     return redirect(url_for('get_all_users'))
+
+
+
+@app.route('/dashboard/users')
+@login_required
+def get_all_users():
+   
+    if current_user.is_authenticated:
+        page = request.args.get('page', 1, type=int)
+        per_page = 4
+        users, total_users = get_data_users(page,per_page)    
+        total_pages = (total_users + per_page - 1) // per_page
+        print(current_user.id)
+        return render_template("user.html", all_users=users,logged_in = current_user.is_authenticated,login_id = current_user.id,current_page=page,
+        total_pages=total_pages)
+
+    return render_template("login.html", all_users=users,logged_in = current_user.is_authenticated)
+
+
+@app.route('/dashboard/users/adduser',methods=['GET','POST'])
+@login_required
+def add_user():
+    
+    if request.method == "POST":
+         
+        # try:
+            email=request.form.get('email')
+
+            user = check_email_exists(email)
+
+            if user:
+                flash("You've already signed up with that email, log in instead!")
+
+                return redirect(url_for("add_user"))
+
+            
+            
+            first_name = request.form["FirstName"]
+            last_name = request.form["LastName"]
+            date_of_birth = request.form["date_of_birth"]
+            gender = request.form["gender"]
+            phone = request.form["Phone"]
+            if not phone.isdigit() or len(phone) != 10:
+                flash(f"An error occurred: {e}")
+                return redirect(url_for("register"))
+            address = request.form["Address"]
+            password = generate_password_hash(request.form["password"],method='pbkdf2:sha256',salt_length=8)
+
+            insert_data_user(first_name,last_name,date_of_birth,gender,email,password,phone,address)
+
+            return redirect(url_for("get_all_users"))
+
+    return render_template("adduser.html")
+
+
+@app.route('/dashboard/users/edituser/<int:user_id>',methods=['GET','POST'])
+@login_required
+def edit_user(user_id):
+    
+    user = get_user_data(user_id)
+
+    if request.method == "POST":
+
+        first_name = request.form["FirstName"]
+        last_name = request.form["LastName"]
+        date_of_birth = user['dob']
+        gender = request.form["gender"]
+        phone = request.form["Phone"]
+        address = request.form["Address"]
+
+        update_data_user(user_id,first_name,last_name,date_of_birth,gender,user['email'],user['password'],phone,address)
+
+        return redirect(url_for("get_all_users"))
+
+    print(user)
+    
+    print(user['email'])
+    return render_template("edituser.html", user=user)
+
 
 if __name__=="__main__":
     app.run(debug=True)
